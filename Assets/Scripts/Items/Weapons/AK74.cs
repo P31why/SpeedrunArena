c6776154e7aba7a0dc58e0403272af48703473c4
MindeRe <MindeRe@DESKTOP-CND4IL3>
2025-02-18 12:55:38 +0300

    added ak74 script

---
+++ Assets/Scripts/Items/Weapons/AK74.cs
@@ -0,0 +1,16 @@
+using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AK74 : MonoBehaviour,IInteractableItem
+{
+    public void PickItem()
+    {
+
+    }
+    public void UseItem()
+    {
+
+    }
+}
