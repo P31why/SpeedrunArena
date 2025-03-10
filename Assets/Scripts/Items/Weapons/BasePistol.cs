76f7df5a0859c2272139911a6730b524df935cf5
MindeRe <MindeRe@DESKTOP-CND4IL3>
2025-03-10 23:07:28 +0300

    updated script

--- Assets/Scripts/Items/Weapons/BasePistol.cs
+++ Assets/Scripts/Items/Weapons/BasePistol.cs
@@ -1,28 +1,50 @@
 using UnityEngine;
 using Assets.Scripts;
+using System.Collections;
 
-public class BasePistol : MonoBehaviour,IInteractableItem
+public class BasePistol : MonoBehaviour, IEquipbleItem,IGun
 {
     [SerializeField]
     private GameObject _playerHand;
-    private bool inInventory;
-    public void Interact()
+    [SerializeField]
+    private GameObject _bullet;
+    [SerializeField]
+    private Transform _shootpoint;
+    [SerializeField]
+    private float _shootForce;
+
+    [SerializeField]
+    private float _shootTime;
+    public void Equip()
     {
-        inInventory = true;
         gameObject.GetComponent<Rigidbody>().isKinematic = true;
         gameObject.transform.parent = _playerHand.transform;
         gameObject.transform.position = _playerHand.transform.position;
-       // gameObject.transform.rotation = _playerHand.transform.rotation;
-        gameObject.transform.localRotation =  Quaternion.Euler(Vector3.forward);
+        gameObject.transform.rotation = _playerHand.transform.rotation;
+       // gameObject.transform.localRotation =  Quaternion.Euler(Vector3.forward.normalized);
     }
-    private void Update()
+    public void Drop()
     {
-        if(inInventory && Input.GetKeyDown(KeyCode.Q))
+        gameObject.transform.parent = null;
+        gameObject.GetComponent<Rigidbody>().isKinematic = false;
+        //gameObject.GetComponent<Rigidbody>().AddForce(Vector3.forward * 100);
+    }
+    public void Shoot()
+    {
+        if (_shootTime <= 0)
         {
-            inInventory = false;
-            gameObject.transform.parent = null;
-            gameObject.GetComponent<Rigidbody>().isKinematic = false;
-            //gameObject.GetComponent<Rigidbody>().AddForce(Vector3.forward * 100);
+            GameObject obj = Instantiate(_bullet, _shootpoint.position, Quaternion.identity);
+            obj.GetComponent<Rigidbody>().AddForce(Vector3.forward * _shootForce,ForceMode.Force);
+            _shootTime = 3;
+            StartCoroutine(CheckTime());
+        }
+    }
+    IEnumerator CheckTime()
+    {
+        while (_shootTime > 0)
+        {
+            yield return new WaitForSeconds(0.1f);
+            _shootTime -= 1;
         }
     }
 }
